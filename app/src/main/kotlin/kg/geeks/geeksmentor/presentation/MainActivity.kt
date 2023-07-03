738f4240b6f0b904302ae0cfd006795c68b9ae2f
itishn1k <kayrat1224mamataev@gmail.com>
2023-07-03 23:13:41 +0600

    nothing special

--- app/src/main/kotlin/kg/geeks/geeksmentor/presentation/MainActivity.kt
+++ app/src/main/kotlin/kg/geeks/geeksmentor/presentation/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -13,9 +12,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import kg.geeks.geeksmentor.R
 import kg.geeks.geeksmentor.databinding.ActivityMainBinding
 
-@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
-//    private lateinit var pref: Pref
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,8 +20,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val navView: BottomNavigationView = binding.navView
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
+        val navView: BottomNavigationView = binding.navView
         val navController = navHostFragment.navController
 
         navController.navigate(R.id.onBoardFragment)
@@ -42,23 +39,27 @@ class MainActivity : AppCompatActivity() {
             R.id.navigation_search,
             R.id.navigation_profile
         )
-        navController.addOnDestinationChangedListener { controller, destination, arguments ->
+        navController.addOnDestinationChangedListener { _, destination, _ ->
             navView.isVisible = bottomNavFragments.contains(destination.id)
             if (destination.id == R.id.onBoardFragment) {
-                window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+                window.setFlags(
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS,
+                    WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS
+                )
             } else {
                 window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
             }
         }
     }
 
-    override fun onBackPressed() {
-        val navController = findNavController(R.id.nav_host_fragment_activity_main)
-
-        if (navController.currentDestination?.id == R.id.navigation_home) {
-            finish() // Выход из активити или приложения, только если текущий фрагмент - navigation_home
-        } else {
-            super.onBackPressed() // Обычное поведение кнопки "Назад" для других фрагментов
-        }
-    }
+//    @Deprecated("Deprecated in Java")
+//    override fun onBackPressed() {
+//        val navController = findNavController(R.id.nav_host_fragment_activity_main)
+//
+//        if (navController.currentDestination?.id == R.id.navigation_home) {
+//            finish() // Выход из активити или приложения, только если текущий фрагмент - navigation_home
+//        } else {
+//            super.onBackPressed() // Обычное поведение кнопки "Назад" для других фрагментов
+//        }
+//    }
 }
